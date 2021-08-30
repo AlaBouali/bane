db8efb02b24e742f4bf7ad93eebe3589a3f6f215
AlaBouali <trap.leader.123@gmail.com>
2021-08-30 06:42:24 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ if (sys.platform == "win32") or( sys.platform == "win64"):
 
 setuptools.setup(
     name="bane",
-    version="4.7.0",
+    version="4.7.1",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library",
