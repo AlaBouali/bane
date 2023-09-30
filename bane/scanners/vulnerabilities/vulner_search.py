84d5fb43eed08c993a21bc94869bbe22a8128113
AlaBouali <ala.bouali.1997@gmail.com>
2023-09-30 06:38:18 +0200

    added new feature to README.md

--- bane/scanners/vulnerabilities/vulner_search.py
+++ bane/scanners/vulnerabilities/vulner_search.py
@@ -70,6 +70,8 @@ def vulners_search(
                 )
                 i += 1
             return l
+        else:
+            return {'error':"couldn't find vulnerabilities for this version"}
     except:
         pass
     return []
