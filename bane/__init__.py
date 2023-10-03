889b4d8dd94f5c5bd3295e1a3ebea72457ca2890
AlaBouali <ala.bouali.1997@gmail.com>
2023-10-03 09:07:08 +0200

    fix version

--- bane/__init__.py
+++ bane/__init__.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 
 
-__version__='4.9.4'
+__version__='4.9.5'
 
 __author__="Ala Bouali"
 
--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ if platform.system()=='Java':
 
 setuptools.setup(
     name="bane",
-    version="4.9.4",
+    version="4.9.5",
     author="AlaBouali",
     author_email="trap.leader.123@gmail.com",
     description="cyber security library, penetration testing module",
