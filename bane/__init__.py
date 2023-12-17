15a391a8e514d74227cbd8d99fa7ba9e26db2afb
AlaBouali <ala.bouali.1997@gmail.com>
2023-12-17 16:45:23 +0100

    correcting NGINX's CPE + new version

--- bane/__init__.py
+++ bane/__init__.py
@@ -1,7 +1,7 @@
 #coding: utf-8
 
 
-__version__='5.1.5'
+__version__='5.1.8'
 
 __author__="Ala Bouali"
 
--- bane/scanners/web_servers/nginx_scanner.py
+++ bane/scanners/web_servers/nginx_scanner.py
@@ -4,4 +4,4 @@ class Nginx_Server_Scanner:
 
     @staticmethod
     def scan(version,**kwargs):
-        return Vulners_Search_Scanner.scan_cpe('nginx','nginx',version=version,**kwargs)
+        return Vulners_Search_Scanner.scan_cpe('f5','nginx',version=version,**kwargs)
